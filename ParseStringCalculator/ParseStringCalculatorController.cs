a0f97f5baa6c9d0e5f4cc07a9d07239a664e7891
ProNinjaDev <pronin-04@bk.ru>
2024-12-16 23:40:23 +0300

    Expand the program, add new operators, new tests
    - Implement support for converting infix expression record to postfix (reverse Polish record).
    - Add a method for calculating a postfix expression with support for the following operations: +, -, *, /, ^ and unary minus (~).
    - Extend number parsing method to work with fractional numbers.
    - Improve readability and code structure.
    - Add new tests for new operators
    - Add longer checks in tests

--- ParseStringCalculator/ParseStringCalculatorController.cs
+++ ParseStringCalculator/ParseStringCalculatorController.cs
@@ -10,22 +10,129 @@ namespace ParseStringCalculator
     public class ParseStringCalculatorController (string expression)
     {
         private string[] _expression = expression.Split();
+        private string InfixExpression = expression;
+        private string PostfixExpression;
+
+        private readonly Dictionary<char, int> operationPriority = new() 
+        {
+            { '(', 0 },
+            { '+', 1 },
+            { '-', 1 },
+            { '*', 2 },
+            { '/', 2 },
+            { '^', 3 },
+            { '~', 4 }
+        };
+
+        public string PostfixExpression1 { get => PostfixExpression2; set => PostfixExpression2 = value; }
+        public string PostfixExpression2 { get => PostfixExpression; set => PostfixExpression = value; }
 
         public string Calculate()
         {
-            var expressionBlocks = expression.Split(' ');
+            string postfixExpression = ToPostfix(expression + "\r");
+            Stack<double> stack = new();
 
-            var num1 = double.Parse(expressionBlocks[0]);
-            var num2 = double.Parse(expressionBlocks[2]);
-            var oper = expressionBlocks[1];
+            string[] operators = postfixExpression.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            switch (oper)
+            foreach (string oper in operators)
             {
-                case "+":
-                    return (num1 + num2).ToString();
-                default:
-                    throw new ArgumentException("Unknown operation");
+                if (double.TryParse(oper, out double number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    double operand2 = stack.Pop();
+                    double operand1 = stack.Count > 0 ? stack.Pop() : 0;
+
+                    switch (oper)
+                    {
+                        case "+":
+                            stack.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            stack.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            stack.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            stack.Push(operand1 / operand2);
+                            break;
+                        case "^":
+                            stack.Push(Math.Pow(operand1, operand2));
+                            break;
+                        case "~":
+                            stack.Push(-operand2);
+                            break;
+                        default:
+                            throw new ArgumentException($"Unknown operator: {oper}");
+                    }
+                }
             }
+            return stack.Pop().ToString();
+        }
+
+        private string ToPostfix(string infixExpression)
+        {
+            string postfixExpression = "";
+            Stack<char> stack = new();
+
+            for (int i = 0; i < infixExpression.Length; i++)
+            {
+                char c = infixExpression[i];
+
+                if (char.IsDigit(c))
+                {
+                    postfixExpression += GetStringNumber(infixExpression, ref i) + " ";
+                }
+                else if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                        postfixExpression += stack.Pop();
+                    stack.Pop();
+                }
+                else if (operationPriority.ContainsKey(c))
+                {
+                    char op = c;
+
+                    if (op == '-' && (i == 0 || (i > 1 && operationPriority.ContainsKey(infixExpression[i - 1]))))
+                        op = '~';
+
+                    while (stack.Count > 0 && operationPriority[stack.Peek()] >= operationPriority[op])
+                        postfixExpression += stack.Pop();
+                    stack.Push(op);
+                }
+            }
+
+            foreach (char op in stack)
+                postfixExpression += op;
+
+            return postfixExpression;
+        }
+
+        private string GetStringNumber(string infixExpression, ref int i)
+        {
+            string strNumber = "";
+
+            for (; i < infixExpression.Length; i++)
+            {
+                char num = infixExpression[i];
+
+                if (char.IsDigit(num))
+                    strNumber += num;
+                else
+                {
+                    i--;
+                    break;
+                }
+            }
+
+            return strNumber;
         }
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -5,12 +5,18 @@ namespace ParseCalculatorTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("2 + 2", "4")]
+        [InlineData("3 * 3", "9")]
+        [InlineData("10 - 5", "5")]
+        [InlineData("15 / 3", "5")]
+        [InlineData("2 + 2 * 2", "6")]
+        [InlineData("(2 + 2) * 2", "8")]
+        public void CalculatorTest(string expression, string expected)
         {
-            var calculator = new ParseStringCalculatorController("2 + 2");
+            var calculator = new ParseStringCalculatorController(expression);
             var result = calculator.Calculate();
-            Assert.Equal("4", result);
+            Assert.Equal(expected, result);
 
         }
     }
